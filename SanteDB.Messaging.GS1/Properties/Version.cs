e2689193aa50549ca6146ec38dc2ba9132c1e385
Justin Fyfe <justin@fyfesoftware.ca>
2021-07-04 18:37:48 -0400

    Updated the MDM layer to emit more descriptive errors

--- SanteDB.Messaging.GS1/Properties/Version.cs
+++ SanteDB.Messaging.GS1/Properties/Version.cs
@@ -7,7 +7,8 @@
 //     the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
-[assembly: System.Reflection.AssemblyFileVersion("2.1.28")]
-[assembly: System.Reflection.AssemblyVersion("2.1.28")]
+
+[assembly: System.Reflection.AssemblyFileVersion("2.1.30")]
+[assembly: System.Reflection.AssemblyVersion("2.1.30")]
 
 

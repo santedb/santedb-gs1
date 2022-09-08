11c490d92a5d924fa255a10d8e0b6a7549a9a92a
Justin Fyfe <justin@fyfesoftware.ca>
2022-09-08 11:14:34 -0400

    Updated license headers

--- SanteDB.Messaging.GS1/Configuration/Gs1ConfigurationSection.cs
+++ SanteDB.Messaging.GS1/Configuration/Gs1ConfigurationSection.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Portions Copyright 2019-2021, Fyfe Software Inc. and the SanteSuite Contributors (See NOTICE)
+ * Portions Copyright 2019-2022, Fyfe Software Inc. and the SanteSuite Contributors (See NOTICE)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you 
  * may not use this file except in compliance with the License. You may 
@@ -13,8 +13,8 @@
  * License for the specific language governing permissions and limitations under 
  * the License.
  * 
- * User: fyfej (Justin Fyfe)
- * Date: 2021-8-5
+ * User: fyfej (<Unknown>)
+ * Date: 2022-5-30
  */
 using SanteDB.Core.Configuration;
 using System;
